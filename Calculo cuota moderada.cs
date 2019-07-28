e724ec0ad7355a4cbbd63bddd68b51210365eea5
Dhako197 <53204420+Dhako197@users.noreply.github.com>
2019-07-28 15:41:55 -0500

    Add files via upload

---
+++ Calculo cuota moderada.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_cuota_moderada2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de su salario mensual:");
+            Double sal = Double.Parse(Console.ReadLine());
+            Double smmlv = 828116;
+
+            if (sal< smmlv*2)
+            {
+                Console.WriteLine("Paga tarifa A.");
+                Console.WriteLine("Valor de cuota moderadora: $3200");
+            }
+
+            else if ((smmlv*2) <= sal && sal <=(smmlv *5))
+            {
+                Console.WriteLine("Paga tarifa B");
+                Console.WriteLine("Valor de cuota moderadora: $12700 ");
+            }
+
+            else
+            {
+                Console.WriteLine("Paga tarifa C");
+                Console.WriteLine("Valor de cuota moderadora: $33500");
+            }
+        }
+    }
+}
---
+++ Calculo de tarifa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_calculo_de_tarifa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de su salario:");
+            double sal = double.Parse(Console.ReadLine());
+
+            if (sal < 828116 * 2)
+            {
+                Console.WriteLine("Se paga tarifa A");
+            }
+
+            else if ((828116 * 2) <= sal && sal <=(828116 * 4))
+            {
+                Console.WriteLine("Se paga tarifa B");
+            }
+
+            else
+            {
+                Console.WriteLine("Paga tarifa c");
+            } 
+
+        }
+    }
+}
