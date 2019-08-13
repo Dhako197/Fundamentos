52d7f9a96bbadfa93391f38c0a47524264200746
Dhako197 <53204420+Dhako197@users.noreply.github.com>
2019-08-12 22:42:06 -0500

    Add files via upload

---
+++ 21.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace black_jack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int carta1 = aleatorio.Next(1, 11);
+            int carta2 = aleatorio.Next(1, 11);
+            int otraCarta;
+            string continuar = "s";
+            Console.WriteLine("Primera carta: " + carta1);
+            Console.WriteLine("Segunda carta: " + carta2);
+            int total = carta1 + carta2;
+            Console.WriteLine("Total: " + total);
+
+            Console.WriteLine("Desea otra carta? (s/n)");
+            continuar = Console.ReadLine();
+
+            while (continuar=="s" && total < 21)
+            {
+                otraCarta = aleatorio.Next(1, 11);
+                Console.WriteLine("Nueva carta:" + otraCarta);
+                total += otraCarta;
+                if (total <= 21)
+                {
+                    Console.WriteLine("Nuevo total:" + total);
+                    Console.WriteLine("Desea otra carta? (s/n)");
+                    continuar = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Has perdido");
+                }
+            }
+
+            Console.WriteLine("Tu total final:" + total);
+
+        }
+    }
+}
