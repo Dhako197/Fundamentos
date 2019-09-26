17cac9a7be14346612fa3c9c4e2d5b516b9d99bf
Dhako197 <53204420+Dhako197@users.noreply.github.com>
2019-09-26 08:02:50 -0500

    Add files via upload

---
+++ EJ2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ2 : MonoBehaviour
+{
+    Renderer render;
+    float t = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        Color a = new Color(1, 0, 0);
+        Color b = new Color(0, 0.63f, 0.90f);
+        Color c = new Color(0.25f, 0, 0.50f);
+
+        if (t < 3) render.material.color = a;
+        else if (t >= 3 && t < 6) render.material.color = b;
+        else render.material.color = c;
+
+    }
+}
---
+++ Ej3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej3 : MonoBehaviour
+{
+
+    Light luz;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        luz = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color a = new Color(1, 0, 0);
+        Color b = new Color(0, 0.63f, 0.90f);
+        Color c = new Color(1, 1, 1);
+
+        if (luz.intensity > 0.25 && luz.intensity < 0.5) luz.color = b;
+        else if (luz.intensity > 0.5 && luz.intensity < 0.75) luz.color = a;
+        else if (luz.intensity >= 0.75) luz.color = c;
+    }
+}
