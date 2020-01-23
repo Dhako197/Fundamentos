f8d2801cc83199fac98bbb538b4913d3b8a8008a
Dhako197 <53204420+Dhako197@users.noreply.github.com>
2020-01-23 17:48:10 -0500

    Add files via upload
    poo

--- Program.cs
+++ Program.cs
@@ -4,41 +4,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace repaso
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese frase");
-            string frase = Console.ReadLine();
-            Console.WriteLine("Ingrese palabra a buscar");
-            string palabra = Console.ReadLine();
 
-            string fraseMayus = frase.ToUpper();
-            string palabraMayus = palabra.ToUpper();
-            int veces = 0;
-           
+            bool esNumero;
+            int opt;
+            int[] miVector = null;
 
-
-            for (int i=0; i<fraseMayus.Length - palabraMayus.Length+1; i++)
+            do
             {
-                string palabraBuscada = "";
-                for (int j = 0; j < palabraMayus.Length; j++)
+                mostrarMenu();
+                do
                 {
-                    palabraBuscada += fraseMayus[i+j];
-                    //Console.Write(palabraBuscada);                   
-                }
-                if (palabraBuscada == palabraMayus)
+                    Console.WriteLine("Ingrese una opcion");
+                    esNumero = int.TryParse(Console.ReadLine(), out opt);
+                } while (!esNumero);
+
+                switch (opt)
                 {
-                    Console.WriteLine("Palabra encontrada en: " + i );
-                    veces++;
+                    case 0: //salir
+                        Console.WriteLine("Chao");
+                        break;
+
+                    case 1: //Ingresar valor
+                        miVector = ingresarDatos();
+                        break;
+
+                    case 2: // ordeanr datos
+                        break;
+
+                    case 3: // Buscar numero
+                        break;
+
+                    case 4: // Remplazar elemto
+                        break;
+
+                    default:
+                        Console.WriteLine("Oh, algo salio mal");
+                        break;
+
                 }
 
+
+
+            } while (opt != 0);
+
+
+        }
+
+        public static void mostrarMenu()
+        {
+            Console.WriteLine(" Aplicacion de vectores");
+            Console.WriteLine("0, Salir");
+            Console.WriteLine("1, Ingresar datos del vector");
+            Console.WriteLine("2, Ordenar datos");
+            Console.WriteLine("3, Busacar numero en vector");
+            Console.WriteLine("4, Remplazar un elemento");
+
+
+        }
+        // zona de subrutinas
+
+        public static int[] ingresarDatos()
+        {
+            int[] miVector = new int[5];
+            int nro = 0;
+            bool esNumero = false;
+
+            for (int i =0; i< 5; i++)
+            {
+                Console.WriteLine("Ingrese una valor para la posicion "+ i + ":" );
+                esNumero = int.TryParse(Console.ReadLine(), out nro);
+                if (esNumero)
+                    miVector[i] = nro;
+                else
+                    miVector[i] = 0;
 
             }
-            Console.WriteLine("Veces que se encontro: " + veces);
-            
+
+
+            return miVector;
         }
     }
 }
